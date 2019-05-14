6fb405e34e639deec19fadfc7be21a0a152e6f5b
ttongw <2608419018@qq.com>
2019-05-14 08:45:39 +0800

    index

---
+++ 华夏服章/华夏服章/Attribute/LoginAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 华夏服章.Attribute
+{
+        public class LoginAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                if (filterContext.HttpContext.Session["UsrId"] == null)
+                {
+                    ContentResult cr = new ContentResult();
+                    cr.Content = "<script>alert('您尚未登陆，请登陆'); window.window.location.href = '/Usrs/Login';</script>"; /*$('.denglubox').css('display', 'block');*/
+                    filterContext.Result = cr;
+                }
+            }
+        }
+}
---
+++ 华夏服章/华夏服章/Controllers/CommunityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 华夏服章.Controllers
+{
+    public class CommunityController : Controller
+    {
+        // GET: Community
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
